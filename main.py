95d38c2ee762425a3679a5e5f01335f7d962974f
parameshwar-dunuka <55077197+parameshwar-dunuka@users.noreply.github.com>
2026-05-02 18:20:12 +0000

    new changes for port

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello FastAPI"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
